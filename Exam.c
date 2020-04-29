509ffef980fff1f1ba8acaa8bba3c13fe5a7f9ed
Em3 <37823382+EricMzky@users.noreply.github.com>
2020-04-29 09:50:54 +0800

    以C为模板，API注释，.h文件分类。慢慢总结加油~！

---
+++ Exam.c
@@ -0,0 +1,25 @@
+/********************************************************************
+*XXXXXX System Development XXXXX.XXXX Software
+*Copyright (c) 2019-2029, XXX LTD., All rights reserved.
+*File Name:   XXXXXXX.c
+*File Description: Implementation of  XXXXXXX.c
+*Modification History:
+*v1.0.0: 
+	*a	2019/08/19  15:43:08 Em3	Created
+*v2.0.0:
+	*a	2019/08/19  15:43:08 Em3	Created
+********************************************************************/
+
+
+/********************************************************************
+* Function: 	XXXX_XXXX
+* Description:  XXXXX
+* Parameter:	void
+	*input 	:
+	*output :
+* Return:    	No Return
+* Note:
+* Warning:
+* History:
+*a	 2019/08/19	15:43:08  Em3	Created
+********************************************************************/
---
+++ Exam.h
@@ -0,0 +1,69 @@
+/********************************************************************
+*XXXXXX System Development XXXXX.XXXX Software
+*Copyright (c) 2019-2029, XXX LTD., All rights reserved.
+*E-Mail：eric.mzky@foxmail.com
+*File Name:   XXXXX.h
+*File Description: Implementation of  XXXXX.h
+*Modification History:
+*a	 19-08-2019 15:43:08	Em3  Created
+********************************************************************/
+
+#ifndef _XXX_XXXXXXXXX_H_INCLUDE_
+#define _XXX_XXXXXXXXX_H_INCLUDE_
+
+
+#include "xxxxxxx.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#pragma  pack()
+
+/********************************DEFINE*****************************/
+
+
+
+
+/********************************ENUM******************************
+typedef enum _XXXX_XX_ENUM
+{
+	xx,
+	xx,
+	
+} XXXX_XX_ENUM;*/
+
+
+
+/********************************STRUCT****************************
+typedef struct _XxxxxXxxxx_t
+{
+	xxx;
+	xxx;
+	
+} XxxxxXxxxx_t;*/
+
+
+
+/********************************DECLAR*****************************/
+/*void*/
+
+
+/*BOOL*/
+
+
+/*UINT8*/
+
+
+/*UINT16*/
+
+
+/*UINT32*/
+
+
+#pragma  pack()
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*!defined(_XX_XXXX_XXXXXXXXXX_H_INCLUDE_)*/
+
